55aa2a615663862e1fe8a368ebc296e1c1401173
Christopher McComas <mccomas.chris@gmail.com>
2022-04-27 08:32:31 -0400

    update handicap next year to playoff

--- resources/views/_parts/handicap-table.blade.php
+++ resources/views/_parts/handicap-table.blade.php
@@ -25,7 +25,7 @@
 				<td class="text-center">{{ $player->hc_fourth }}</td>
 				<td class="text-center">{{ $player->hc_playoff }}</td>
 				<td class="text-center">{{ $player->hc_18 }}</td>
-				<td class="text-center">{{ $player->hc_next_year }}</td>
+				<td class="text-center">{{ $player->hc_playoff }}</td>
 				<td class="text-center">{{ number_format($player->hc_full, 4, '.', ',') }}</td>
 				<td class="text-center" width="5%">{{ $player->hc_full_rank }}</td>
                 @admin
