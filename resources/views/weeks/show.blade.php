8913e7def9f84ce58b7a6398dd3d5cb4b637529f
Christopher McComas <mccomas.chris@gmail.com>
2022-06-16 10:19:35 -0400

    add week order to schedule page

--- resources/views/weeks/show.blade.php
+++ resources/views/weeks/show.blade.php
@@ -6,7 +6,7 @@
     <div class="flex flex-col">
         @foreach ($weeks as $week)
             <div class="flex items-center mx-auto mb-4 mt-4">
-                <span class="text-center text-lg font-semibold">{{ date('F d, Y', strtotime($week->week_date)) }} Matchups </span>
+                <span class="text-center text-lg font-semibold">Week {{ $week->week_order }} - {{ date('F d, Y', strtotime($week->week_date)) }} Matchups </span>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" class="fill-current  h-4 w-4"><path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z"/></svg>
                 <span class="text-center text-lg font-semibold">{{ $week->side_games }}</span>
             </div>
