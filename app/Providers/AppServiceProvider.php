c5c0a906fe2b59a66a9a5bedcc3a2ffe18787c73
Christopher McComas <mccomas.chris@gmail.com>
2022-07-14 10:27:43 -0400

    wip

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -33,7 +33,7 @@ class AppServiceProvider extends ServiceProvider
             $this->dispatchBrowserEvent('notify', $message);
         });
 
-        // View::share('activeYear', Year::where('active', 1)->first());
+        View::share('activeYear', Year::where('active', 1)->first());
 
         Blade::if('admin', function () {
             if (Auth::user() && Auth::user()->isAdmin()) {
