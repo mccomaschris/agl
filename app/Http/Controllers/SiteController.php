006e70901923e26a4a1daa13af1c3a4d8679fbbf
Christopher McComas <mccomas.chris@gmail.com>
2022-09-01 21:23:13 -0400

    wip

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -19,18 +19,19 @@ class SiteController extends Controller
     {
         $year = Year::where('active', 1)->first();
 
-        $week = Week::with(
-                'team_a', 'team_b',
-                'team_c', 'team_d',
-                'team_e', 'team_f')
-                ->where('year_id', $year->id)
-                ->where('week_date', '>', Carbon::yesterday())
-                ->first();
+        // $week = Week::with(
+        //         'team_a', 'team_b',
+        //         'team_c', 'team_d',
+        //         'team_e', 'team_f')
+        //         ->where('year_id', $year->id)
+        //         ->where('week_date', '>', Carbon::yesterday())
+        //         ->first();
 
+        $week = null;
 
         $last_week = Week::where('year_id', $year->id)->where('week_date', '<', Carbon::today())->orderBy('week_date', 'desc')->first();
 
-        return view('site.index', compact('year', 'week', 'last_week'));
-        // return view('site.playoff', compact('year', 'last_week'));
+        // return view('site.index', compact('year', 'week', 'last_week'));
+        return view('site.playoff', compact('year', 'last_week'));
     }
 }
