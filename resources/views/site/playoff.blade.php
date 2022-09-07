3a1b5f1137d5a6a0a48e64cb6d11962b1e3b09e8
Christopher McComas <mccomas.chris@gmail.com>
2022-09-07 10:16:07 -0400

    add semifinals

--- resources/views/site/playoff.blade.php
+++ resources/views/site/playoff.blade.php
@@ -13,9 +13,9 @@
 @endsection
 
 @section('page-heading')
-{{-- <div class="flex items-center lg:justify-center mx-auto mt-6 lg:mt-8 mb-2 lg:mb-4 text-darkest-grey">
-    <span class="text-lg lg:text-2xl font-bold">Final Matchups</span>
-</div> --}}
+<div class="flex items-center lg:justify-center mx-auto mt-6 lg:mt-8 mb-2 lg:mb-4 text-darkest-grey">
+    <span class="text-lg lg:text-2xl font-bold">Semifinal Matchups</span>
+</div>
 @endsection
 
 @section('jumbotron')
@@ -24,42 +24,75 @@
 
 @section('content')
 <div class="flex flex-wrap mb-4">
-	{{-- <div class="w-full lg:w-1/3"></div>
+	<div class="w-full lg:w-1/3"></div>
     <div class="w-full lg:w-1/3 mb-4 lg:pr-4">
         <table class="table w-full mx-auto table-striped">
             <thead>
                 <tr class="course">
-                    <th class="text-center rounded-tl" colspan="2">Team 4</th>
+                    <th class="text-center rounded-tl" colspan="2">Team 2</th>
                     <th class="text-center rounded-tr" colspan="2">Team 1</th>
                 </tr>
             </thead>
             <tr>
-                <td width="40%">Vance Bunn</td>
-                <td width="10%" class="text-center">2</td>
-                <td width="40%">Rick Reed</td>
+                <td width="40%">Webb</td>
+                <td width="10%" class="text-center">0</td>
+                <td width="40%">Reed</td>
                 <td width="10%" class="text-center">-1</td>
             </tr>
             <tr class="border-b border-grey-900">
-                <td class="">Craig Edwards</td>
-                <td class="text-center">7</td>
-                <td class="">Danny Freeman</td>
+                <td class="">Barker</td>
+                <td class="text-center">3</td>
+                <td class="">Hamlin</td>
                 <td class="text-center">5</td>
             </tr>
             <tr>
-                <td width="40%">Tyler Barker</td>
+                <td width="40%">C. McComas</td>
+                <td width="10%" class="text-center">7</td>
+                <td width="40%">Edwards</td>
                 <td width="10%" class="text-center">4</td>
-                <td width="40%">Sean Adkins</td>
-                <td width="10%" class="text-center">8</td>
             </tr>
             <tr>
-                <td width="40%">Mark Cutlip</td>
-                <td width="10%" class="text-center">13</td>
-                <td width="40%">Tres Baumgarner</td>
-                <td width="10%" class="text-center">9</td>
+                <td width="40%">Cutlip</td>
+                <td width="10%" class="text-center">12</td>
+                <td width="40%">Thornburg</td>
+                <td width="10%" class="text-center">12</td>
             </tr>
         </table>
     </div>
-    <div class="w-full lg:w-1/3"></div> --}}
+    <div class="w-full lg:w-1/3">
+        <table class="table w-full mx-auto table-striped">
+            <thead>
+                <tr class="course">
+                    <th class="text-center rounded-tl" colspan="2">Team 5</th>
+                    <th class="text-center rounded-tr" colspan="2">Team 6</th>
+                </tr>
+            </thead>
+            <tr>
+                <td width="40%">Prater</td>
+                <td width="10%" class="text-center">4</td>
+                <td width="40%">Keeny</td>
+                <td width="10%" class="text-center">3</td>
+            </tr>
+            <tr class="border-b border-grey-900">
+                <td class="">Freeman</td>
+                <td class="text-center">4</td>
+                <td class="">Cartwright</td>
+                <td class="text-center">5</td>
+            </tr>
+            <tr>
+                <td width="40%">T. Mills</td>
+                <td width="10%" class="text-center">5</td>
+                <td width="40%">Conard</td>
+                <td width="10%" class="text-center">8</td>
+            </tr>
+            <tr>
+                <td width="40%">Hedrick</td>
+                <td width="10%" class="text-center">11</td>
+                <td width="40%">R. Mills</td>
+                <td width="10%" class="text-center">12</td>
+            </tr>
+        </table>
+    </div>
 </div>
 <div class="flex flex-wrap lg:-mx-4 mb-4">
   <div class="w-full lg:w-2/3 pr-0 lg:px-4 mb-4 lg:mb-0">
