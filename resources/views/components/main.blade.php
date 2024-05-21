a7d941d1031f101231579867795b289a9dc2051f
Christopher McComas <mccomas.chris@gmail.com>
2024-05-21 18:51:39 -0400

    wip

--- resources/views/components/main.blade.php
+++ resources/views/components/main.blade.php
@@ -50,8 +50,8 @@
                             <span class="font-semibold text-white tracking-tight text-2xl">{{ config('app.name') }}</span>
                         </a>
                     </div>
-                    <div class="block lg:hidden relative" x-on:click="open = !open">
-                        <button class="flex items-center px-3 py-2 border rounded border-white hover:text-white hover:border-white">
+                    <div class="block lg:hidden relative">
+                        <button class="flex items-center px-3 py-2 border rounded border-white" x-on:click="open = !open">
                             <svg x-show="open === false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" class="text-white fill-current h-4 w-4"><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
                             <svg x-show="open === true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" class="text-white fill-current h-4 w-4"><path d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z"/></svg>
                         </button>
