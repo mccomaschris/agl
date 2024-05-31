9f7fcad06de0a0ae57cd8a60c0ecd46266433632
Christopher McComas <mccomas.chris@gmail.com>
2024-05-30 21:44:58 -0400

    update npm and other stuff

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,11 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/assets/css/main.css', 'resources/css/app.css', 'resources/js/app.js', 'resources/assets/js/stim.js'],
+            input: [
+							'resources/assets/css/main.css',
+							'resources/css/app.css',
+							'resources/js/app.js'
+            ],
             refresh: true,
         }),
     ],
