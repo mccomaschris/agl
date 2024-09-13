a7905040a88a79256dfdd805b4c1412f5d3f9295
Christopher McComas <mccomas.chris@gmail.com>
2024-09-13 08:45:05 -0400

    wip

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -52,9 +52,9 @@
                         </a>
                     </div>
                     <div class="block lg:hidden relative">
-                        <button class="flex items-center px-3 py-2 border rounded border-white hover:text-white hover:border-white">
-                            <svg x-on:click="open = true" x-show="open === false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" class="text-white fill-current h-4 w-4"><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
-                            <svg x-cloak x-on:click="open = false" x-show="open === true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" class="text-white fill-current h-4 w-4"><path d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z"/></svg>
+						<button class="flex items-center px-3 py-2 border rounded border-white hover:text-white hover:border-white" x-on:click="open = !open">
+                            <svg x-show="open === false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" class="text-white fill-current h-4 w-4"><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+                            <svg x-cloak x-show="open === true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" class="text-white fill-current h-4 w-4"><path d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z"/></svg>
                         </button>
 						<div
 							x-cloak
