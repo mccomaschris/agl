59b2098a01617265dcb1fd60c178393d35b49d49
Christopher McComas <mccomas.chris@gmail.com>
2025-04-18 08:23:56 -0400

    add resetpassword job

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,12 +1,8 @@
-@extends('layouts.default')
-
-@section('content')
-
+<x-layouts.app>
   <h1 class="text-green text-5xl lg:text-7xl text-center mb-2">404</h1>
   <p class="text-center text-green mb-8 lg:mb-16 w-full lg:w-1/3 text-sm lg:text-base uppercase font-bold tracking-wide mx-auto">This page cannot be found better refer to the rulebook on a lost ball.</p>
   <img src="/images/lost-ball.png">
   <div class="mt-6 text-center w-full">
     <a href="/" class="text-sm">Go Home</a>
   </div>
-
-@endsection
+</x-layouts.app>
