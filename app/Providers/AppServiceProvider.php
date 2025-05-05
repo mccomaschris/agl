b5358c7d4fdd55f07b673c14a6d1f146c0386444
Christopher McComas <mccomas.chris@gmail.com>
2025-05-05 11:10:38 -0400

    wip

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -30,10 +30,10 @@ class AppServiceProvider extends ServiceProvider
             $this->dispatchBrowserEvent('notify', $message);
         });
 
-        if (Schema::hasTable('years')) { // ✅ Prevents errors before migration runs
-			$activeYear = Year::where('active', true)->first();
-			view()->share('activeYear', $activeYear);
-		}
+        // if (Schema::hasTable('years')) { // ✅ Prevents errors before migration runs
+		// 	$activeYear = Year::where('active', true)->first();
+		// 	view()->share('activeYear', $activeYear);
+		// }
 
         Blade::if('admin', function () {
             if (Auth::user() && Auth::user()->isAdmin()) {
