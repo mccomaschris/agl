ce9afc9129b6794aa69b160d3b2142f74feb7809
Christopher McComas <mccomas.chris@gmail.com>
2025-05-09 10:50:24 -0400

    wip

--- app/Http/Controllers/WeekController.php
+++ app/Http/Controllers/WeekController.php
@@ -16,11 +16,9 @@ class WeekController extends Controller
      */
     public function show(Year $year): View
     {
-        $weeks = Cache::remember('weeks', 43200, function () use ($year) {
-            return Week::with('team_a', 'team_b', 'team_c', 'team_d', 'team_e', 'team_f')
-                ->where('year_id', $year->id)
-                ->where('week_date', '>', Carbon::yesterday())->get();
-        });
+        $weeks = Week::with('team_a', 'team_b', 'team_c', 'team_d', 'team_e', 'team_f')
+			->where('year_id', $year->id)
+			->where('week_date', '>', Carbon::yesterday())->get();
 
         return view('weeks.show', compact('year', 'weeks'));
     }
