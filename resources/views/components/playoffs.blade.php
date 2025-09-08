0f4b37bfc8ec4613f7113cd723ed8ca23f36846a
Christopher McComas <mccomas.chris@gmail.com>
2025-09-08 10:17:41 -0400

    wip

---
+++ resources/views/components/playoffs.blade.php
@@ -0,0 +1,85 @@
+
+<flux:heading size="lg">Semifinals</flux:heading>
+
+<div class="grid grid-cols-1 lg:grid-cols-3 gap-4">
+	<div class="">
+		<x-table>
+			<x-table.thead>
+				<x-table.tr>
+					<x-table.th colspan="2" class="text-center!">
+						Team 2
+					</x-table.th>
+					<x-table.th colspan="2">
+						Team 6
+					</x-table.th>
+				</x-table.tr>
+			</x-table.thead>
+			<x-table.tbody>
+				<x-table.tr-body class="bg-zinc-50 dark:bg-zinc-800!">
+					<x-table.td class="w-[40%] border-r-0! pl-2! pr-2!">Webb</x-table.td>
+					<x-table.td class="text-center w-[10%] dark:text-zinc-200! border-r border-zinc-200">0</x-table.td>
+					<x-table.td class="w-[40%] border-r-0! pl-2! pr-2! text-left!">Keeney</x-table.td>
+					<x-table.td class="text-center w-[10%] dark:text-zinc-200!">2</x-table.td>
+				</x-table.tr-body>
+				<x-table.tr-body class="bg-zinc-50 dark:bg-zinc-800! border-b-4 border-zinc-500">
+					<x-table.td class="w-[40%] border-r-0! pl-2! pr-2!">Freeman</x-table.td>
+					<x-table.td class="text-center w-[10%] dark:text-zinc-200! border-r border-zinc-200">5</x-table.td>
+					<x-table.td class="w-[40%] border-r-0! pl-2! pr-2! text-left!">Evans</x-table.td>
+					<x-table.td class="text-center w-[10%] dark:text-zinc-200!">5</x-table.td>
+				</x-table.tr-body>
+				<x-table.tr-body class="bg-zinc-50 dark:bg-zinc-800!">
+					<x-table.td class="w-[40%] border-r-0! pl-2! pr-2!">S Adkins</x-table.td>
+					<x-table.td class="text-center w-[10%] dark:text-zinc-200! border-r border-zinc-200">6</x-table.td>
+					<x-table.td class="w-[40%] border-r-0! pl-2! pr-2! text-left!">Keyser</x-table.td>
+					<x-table.td class="text-center w-[10%] dark:text-zinc-200!">7</x-table.td>
+				</x-table.tr-body>
+				<x-table.tr-body class="bg-zinc-50 dark:bg-zinc-800!">
+					<x-table.td class="w-[40%] border-r-0! pl-2! pr-2!">Dishman</x-table.td>
+					<x-table.td class="text-center w-[10%] dark:text-zinc-200! border-r border-zinc-200">7</x-table.td>
+					<x-table.td class="w-[40%] border-r-0! pl-2! pr-2! text-left!">Baumgarner</x-table.td>
+					<x-table.td class="text-center w-[10%] dark:text-zinc-200!">12</x-table.td>
+				</x-table.tr-body>
+			</x-table.tbody>
+		</x-table>
+	</div>
+	<div class="">
+		<x-table>
+			<x-table.thead>
+				<x-table.tr>
+					<x-table.th colspan="2" class="text-center!">
+						Team 1
+					</x-table.th>
+					<x-table.th colspan="2">
+						Team 3
+					</x-table.th>
+				</x-table.tr>
+			</x-table.thead>
+			<x-table.tbody>
+				<x-table.tr-body class="bg-zinc-50 dark:bg-zinc-800!">
+					<x-table.td class="w-[40%] border-r-0! pl-2! pr-2!">Reed</x-table.td>
+					<x-table.td class="text-center w-[10%] dark:text-zinc-200! border-r border-zinc-200">-1</x-table.td>
+					<x-table.td class="w-[40%] border-r-0! pl-2! pr-2!  text-left!">Bunn</x-table.td>
+					<x-table.td class="text-center w-[10%] dark:text-zinc-200!">2</x-table.td>
+				</x-table.tr-body>
+				<x-table.tr-body class="bg-zinc-50 dark:bg-zinc-800! border-b-4 border-zinc-500">
+					<x-table.td class="w-[40%] border-r-0! pl-2! pr-2! text-left">Aliff</x-table.td>
+					<x-table.td class="text-center w-[10%] dark:text-zinc-200! border-r border-zinc-200">6</x-table.td>
+					<x-table.td class="w-[40%] border-r-0! pl-2! pr-2! text-left!">T Mills</x-table.td>
+					<x-table.td class="text-center w-[10%] dark:text-zinc-200!">6</x-table.td>
+				</x-table.tr-body>
+				<x-table.tr-body class="bg-zinc-50 dark:bg-zinc-800!">
+					<x-table.td class="w-[40%] border-r-0! pl-2! pr-2!">Dumont</x-table.td>
+					<x-table.td class="text-center w-[10%] dark:text-zinc-200! border-r border-zinc-200">11</x-table.td>
+					<x-table.td class="w-[40%] border-r-0! pl-2! pr-2! text-left!">Chandler</x-table.td>
+					<x-table.td class="text-center w-[10%] dark:text-zinc-200!">8</x-table.td>
+				</x-table.tr-body>
+				<x-table.tr-body class="bg-zinc-50 dark:bg-zinc-800!">
+					<x-table.td class="w-[40%] border-r-0! pl-2! pr-2!">Cutlip</x-table.td>
+					<x-table.td class="text-center w-[10%] dark:text-zinc-200! border-r border-zinc-200">12</x-table.td>
+					<x-table.td class="w-[40%] border-r-0! pl-2! pr-2! text-left!">Thornburg</x-table.td>
+					<x-table.td class="text-center w-[10%] dark:text-zinc-200!">13</x-table.td>
+				</x-table.tr-body>
+			</x-table.tbody>
+		</x-table>
+	</div>
+</div>
